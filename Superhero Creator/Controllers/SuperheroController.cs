bec178c65e6fc2f01a5318ebd567aa64a037d914
Shalini Shrinate <you@example.com>
2020-01-02 14:07:34 -0600

    finished working on migration, controller as per Mike's video now onto creating Superheroes!

---
+++ Superhero Creator/Controllers/SuperheroController.cs
@@ -0,0 +1,91 @@
+﻿using Superhero_Creator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Superhero_Creator.Controllers
+{
+    public class SuperheroController : Controller
+    {
+        // GET: Superhero
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Superhero/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Superhero/Create
+        public ActionResult Create()
+        {
+            Superhero superhero = new Superhero();
+            return View(superhero);
+        }
+
+        // POST: Superhero/Create
+        [HttpPost]
+        public ActionResult Create(Superhero superhero)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Superhero/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Superhero/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Superhero/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Superhero/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
